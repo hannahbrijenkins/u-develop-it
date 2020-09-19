78aa201a7df559ba5e634fdd2083e5b06ac6b419
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-09-19 14:18:55 -0400

    added get, delete, and create functions to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 =require('sqlite3').verbose();
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,16 +7,53 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello Earth'
-//     });
-// });
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+});
 
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is now running on port ${PORT}`);
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 5`, (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE a candidate
+// db.run(`DELETE FROM candidates WHERE id =?`, 2, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+// Create a candidate
+const sql = 'INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)';
+const params = [4, 'Charles', 'LeRoi', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
+
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is now running on port ${PORT}`);
+    });
 });
